4aa94161dbdd0e061e9db92dc2a0152eb1d39c03
Adrian Dapprich <adrian@dappri.ch>
2024-11-04 01:01:40 +0100

    try map export from js

--- website/script.js
+++ website/script.js
@@ -1,23 +1,37 @@
-const svgIpt = document.getElementById("svg-input");
+const svgIn = document.getElementById("svg-input");
+const svgOut = document.getElementById("svg-output");
 const svgContainer = document.getElementById("svg-container");
 
-function handle(e) {
-    const file = svgIpt.files[0];
-    console.log(file);
-
-    // if (svg) {
-    //     svg.remove();
-    //     svg = undefined;
-    // }
+function handleIn(e) {
+    const file = svgIn.files[0];
 
     file.text().then((s) => {
-        // svg = document.createElement("div");
-        // svg.id = "svg-container-inner";
-        // svgContainer.insertAdjacentElement("afterbegin", svg);
         svgContainer.insertAdjacentHTML("afterbegin", s);
-        svgIpt.style.display = "none";
+
+        const buildings = document.getElementsByClassName("mapviz-building");
+        for (const building of buildings) {
+            building.style.fill = "#00ff00";
+        }
     });
 }
 
-svgIpt.addEventListener("change", handle);
+function download(filename, str) {
+    const fileBlob = new Blob([str], { type: 'image/svg+xml' });
+    const url = URL.createObjectURL(fileBlob);
+
+    const link = document.createElement('a');
+    link.setAttribute('href', url);
+    link.setAttribute('download', filename);
+    link.click()
+
+    URL.revokeObjectURL(url)
+}
+
+function handleOut(e) {
+    const str = svgContainer.innerHTML;
+    download("map.svg", str);
+}
+
+svgIn.addEventListener("change", handleIn);
+svgOut.addEventListener("click", handleOut);
 console.log("script finished")
