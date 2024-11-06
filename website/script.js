384a2a0acbb7047ca8969dc96ba007656eaa84dc
alexm <alessiamercuri25@gmail.com>
2024-11-06 01:06:15 +0100

    hide download button until map is loaded, delete previous map when new map is loaded, initialize color pickers from svg instead of hardcoding them

--- website/script.js
+++ website/script.js
@@ -1,21 +1,11 @@
 const uploadBtn = document.getElementById("svg-file");
-const colorPickers = document.getElementsByClassName("color-picker");
+// const colorPickers = document.getElementsByClassName("color-picker");
 const preview = document.getElementById("img-box");
+const downloadBtn = document.getElementById("download-btn");
+const sideMenu = document.getElementById("side-menu");
+const btnContainer = document.getElementById("btn-container");
 //let url = undefined;
 
-const uploadBtnHandler = (e) => {
-    console.log(uploadBtn.files);
-    //if (url) {URL.revokeObjectURL(url)};
-    //url = URL.createObjectURL(uploadBtn.files[0]);
-    //preview.innerHTML = uploadBtn.files[0];
-    const file = uploadBtn.files[0];
-    file.text().then((s) => {
-        preview.insertAdjacentHTML("afterbegin", s);
-        const building = document.getElementsByClassName("mapviz-building");
-        const initialColor = building[0].style.fill;
-
-    });
-};
 
 const generalHandler = (e) => {
     console.log(e);
@@ -33,11 +23,85 @@ const generalHandler = (e) => {
     console.log(objects);
 }
 
-uploadBtn.addEventListener("change", uploadBtnHandler);
-for (const colorPicker of colorPickers) {
-    colorPicker.addEventListener("change", generalHandler);
-    colorPicker.value = "black";
-}
+const createColorPicker = (colorPickerInfo) => {
+    const label = document.createElement("label");
+    label.setAttribute("for", colorPickerInfo.layer);
+    label.innerText = colorPickerInfo.name;
+
+    const input = document.createElement("input");
+    input.setAttribute("id", colorPickerInfo.layer);
+    input.classList.add("color-picker");
+    input.setAttribute("type", "color");
+    input.value = colorPickerInfo.color;
+    input.addEventListener("change", generalHandler);
+
+    const div = document.createElement("div");
+    div.classList.add("menu-layer");
+    div.appendChild(label);
+    div.appendChild(input);
+
+    sideMenu.insertBefore(div, btnContainer);
+};
+
+function analyzeSVGEl(element) {
+
+    const cls = element.classList[0];
+    const color = element.getAttribute("fill");
+    const layer = cls.substring(7);
+    const name = layer.charAt(0).toUpperCase() + layer.slice(1);
+
+    return {
+        "layer": layer,
+        "name": name,
+        "color": color
+    };
+};
+
+function analyzeSVG (svg) {
+    //
+    let svgInfo = {};
+    
+    for (const element of svg.children) {
+        const elInfo = analyzeSVGEl(element);
+        svgInfo[elInfo.layer] = elInfo;
+    };
+
+    return svgInfo;
+};
+
+const uploadBtnHandler = (e) => {
+    // console.log(uploadBtn.files);
+    //if (url) {URL.revokeObjectURL(url)};
+    //url = URL.createObjectURL(uploadBtn.files[0]);
+    //preview.innerHTML = uploadBtn.files[0];
+    const file = uploadBtn.files[0];
+    file.text().then((s) => {
+        if (preview.children.length > 0) {
+            preview.removeChild(preview.children[0]);
+        }
+
+        const oldColorPickers = document.getElementsByClassName("menu-layer");
+        console.log(oldColorPickers);
+        for (const colorPicker of oldColorPickers) {
+            console.log("deleting " + colorPicker.id)
+            sideMenu.removeChild(colorPicker);
+        }
+
+        preview.insertAdjacentHTML("afterbegin", s);
+        const svgEl = document.getElementsByClassName("mapviz-map")[0];
+        const svgInfo = analyzeSVG(svgEl);
+        console.log(svgInfo);        
+
+        
+        for (const colorPickerInfo of Object.values(svgInfo)) {
+            createColorPicker(colorPickerInfo);
+        }
+
+        //const building = document.getElementsByClassName("mapviz-building");
+        //const initialColor = building[0].style.fill;
+        downloadBtn.parentElement.classList.remove("hide");
+    });
+};
 
 function download(filename, str) {
     const fileBlob = new Blob([str], { type: 'image/svg+xml' });
@@ -46,12 +110,17 @@ function download(filename, str) {
     const link = document.createElement('a');
     link.setAttribute('href', url);
     link.setAttribute('download', filename);
-    link.click()
+    link.click();
 
-    URL.revokeObjectURL(url)
+    URL.revokeObjectURL(url);
 }
 
-function handleOut(e) {
-    const str = svgContainer.innerHTML;
+function downloadSVG(e) {
+    const str = preview.innerHTML;
     download("map.svg", str);
 }
+
+//event listeners
+
+uploadBtn.addEventListener("change", uploadBtnHandler);
+downloadBtn.addEventListener("click", downloadSVG);
